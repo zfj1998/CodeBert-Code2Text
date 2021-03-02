11237fdcd3eb4a90ca97e9e80f0214fdfa85c480
zfj <893298592@qq.com>
2021-03-02 16:22:55 +0800

    update cuda configure

--- code/start_train.sh
+++ code/start_train.sh
@@ -10,5 +10,6 @@ train_file=$data_dir/$lang/train.jsonl
 dev_file=$data_dir/$lang/valid.jsonl
 epochs=10
 pretrained_model=microsoft/codebert-base
+cuda_devices=0,1
 
-python run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+python run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs --cuda_devices $cuda_devices
