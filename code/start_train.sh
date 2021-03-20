4ebc3260db7d1e83b6215c1e63df9942162879af
zfj <zfj@whu.edu.cn>
2021-03-20 13:04:30 +0800

    change gitignore

--- code/start_train.sh
+++ code/start_train.sh
@@ -6,10 +6,11 @@ source_length=256
 target_length=128
 data_dir=../dataset
 output_dir=../model/$lang
-train_file=$data_dir/$lang/train.jsonl
-dev_file=$data_dir/$lang/valid.jsonl
-epochs=10
+train_file=$data_dir/$lang/TbCS/train-original.jsonl
+dev_file=$data_dir/$lang/TbCS/dev-original.jsonl
+epochs=40
 pretrained_model=microsoft/codebert-base
+load_model_path=../model/python/checkpoint-best-bleu/pytorch_model.bin
 cuda_devices=0,1
 
-python run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs --cuda_devices $cuda_devices
+python run.py --do_train --do_eval --model_type roberta --load_model_path $load_model_path --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs --cuda_devices $cuda_devices
